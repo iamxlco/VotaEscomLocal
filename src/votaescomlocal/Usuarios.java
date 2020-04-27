bdb21de06b69d523c9b0b6d1e3ef14cc7c0c0e82
Ale <Ale@EVA>
2020-04-27 12:57:11 -0500

    Perfil usuarios

--- src/votaescomlocal/Usuarios.java
+++ src/votaescomlocal/Usuarios.java
@@ -31,8 +31,19 @@ public class Usuarios extends javax.swing.JFrame {
         label1 = new java.awt.Label();
         iconoAdmin = new javax.swing.JLabel();
         iconoVE = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tablaUsuarios = new javax.swing.JTable();
+        btnconsultar = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        cbTipoUsuario = new javax.swing.JComboBox<>();
+        cbTipoDato = new javax.swing.JComboBox<>();
+        jtDato = new javax.swing.JTextField();
+        btnBuscar = new javax.swing.JButton();
+        btnEliminarUsuario = new javax.swing.JButton();
+        btnModificarUsuario = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(1200, 700));
         setResizable(false);
 
         jPanel2.setBackground(new java.awt.Color(16, 104, 163));
@@ -71,21 +82,147 @@ public class Usuarios extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
+        tablaUsuarios.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(71, 136, 179), 3));
+        tablaUsuarios.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
+        tablaUsuarios.setForeground(new java.awt.Color(67, 67, 67));
+        tablaUsuarios.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Usuario", "Nombre", "Código NFC", "Fecha de nacimiento", "Dirección"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(tablaUsuarios);
+
+        btnconsultar.setBackground(new java.awt.Color(96, 182, 240));
+        btnconsultar.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
+        btnconsultar.setForeground(new java.awt.Color(255, 255, 255));
+        btnconsultar.setText("Agregar nuevo usuario");
+        btnconsultar.setBorderPainted(false);
+        btnconsultar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        btnconsultar.setMargin(new java.awt.Insets(14, 14, 14, 14));
+
+        jLabel1.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Consultar usuario");
+        jLabel1.setFocusable(false);
+        jLabel1.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+
+        cbTipoUsuario.setBackground(new java.awt.Color(102, 204, 255));
+        cbTipoUsuario.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
+        cbTipoUsuario.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Selecciona un tipo de usuario", "Alumno", "Profesor", "PAAE" }));
+
+        cbTipoDato.setBackground(new java.awt.Color(102, 204, 255));
+        cbTipoDato.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
+        cbTipoDato.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Selecciona un filtro de búsqueda" }));
+        cbTipoDato.setEnabled(false);
+
+        jtDato.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
+        jtDato.setForeground(new java.awt.Color(102, 102, 102));
+        jtDato.setCaretColor(new java.awt.Color(0, 153, 255));
+        jtDato.setEnabled(false);
+        jtDato.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jtDatoActionPerformed(evt);
+            }
+        });
+
+        btnBuscar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/iconLupa.png"))); // NOI18N
+        btnBuscar.setBorderPainted(false);
+        btnBuscar.setContentAreaFilled(false);
+        btnBuscar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        btnBuscar.setPressedIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/iconLupa.png"))); // NOI18N
+        btnBuscar.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/iconLupa34.png"))); // NOI18N
+
+        btnEliminarUsuario.setBackground(new java.awt.Color(96, 182, 240));
+        btnEliminarUsuario.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
+        btnEliminarUsuario.setForeground(new java.awt.Color(255, 255, 255));
+        btnEliminarUsuario.setText("Eliminar usuario");
+        btnEliminarUsuario.setBorderPainted(false);
+        btnEliminarUsuario.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        btnEliminarUsuario.setMargin(new java.awt.Insets(14, 14, 14, 14));
+
+        btnModificarUsuario.setBackground(new java.awt.Color(96, 182, 240));
+        btnModificarUsuario.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
+        btnModificarUsuario.setForeground(new java.awt.Color(255, 255, 255));
+        btnModificarUsuario.setText("Modificar Usuario");
+        btnModificarUsuario.setBorderPainted(false);
+        btnModificarUsuario.setMargin(new java.awt.Insets(14, 14, 14, 14));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(55, 55, 55)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(btnconsultar)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(cbTipoUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, 183, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(cbTipoDato, javax.swing.GroupLayout.PREFERRED_SIZE, 211, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(jtDato, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(btnBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jLabel1))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 825, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(btnModificarUsuario, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnEliminarUsuario, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(109, 109, 109))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(606, Short.MAX_VALUE))
+                .addGap(26, 26, 26)
+                .addComponent(btnconsultar, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(31, 31, 31)
+                        .addComponent(jLabel1)
+                        .addGap(36, 36, 36)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(cbTipoUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(cbTipoDato, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jtDato, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(68, 68, 68)
+                        .addComponent(btnBuscar)))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(41, 41, 41)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 319, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(107, 107, 107)
+                        .addComponent(btnEliminarUsuario)
+                        .addGap(62, 62, 62)
+                        .addComponent(btnModificarUsuario)))
+                .addContainerGap(78, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jtDatoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jtDatoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jtDatoActionPerformed
 
     /**
      * @param args the command line arguments
@@ -124,9 +261,19 @@ public class Usuarios extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnBuscar;
+    private javax.swing.JButton btnEliminarUsuario;
+    private javax.swing.JButton btnModificarUsuario;
+    private javax.swing.JButton btnconsultar;
+    private javax.swing.JComboBox<String> cbTipoDato;
+    private javax.swing.JComboBox<String> cbTipoUsuario;
     private javax.swing.JLabel iconoAdmin;
     private javax.swing.JLabel iconoVE;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField jtDato;
     private java.awt.Label label1;
+    private javax.swing.JTable tablaUsuarios;
     // End of variables declaration//GEN-END:variables
 }
